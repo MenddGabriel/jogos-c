179c40843e5890036ef2210c9fd8aa8706eac687
menddgabriel <gabrieldesouzamendes@hotmail.com.br>
2021-09-21 05:35:09 -0300

    Validacao de cordenada e jogadas

--- jogoDaVelha/Origem.c
+++ jogoDaVelha/Origem.c
@@ -3,18 +3,88 @@
 #define LINHA 3
 #define COLUNA 3
 
+char tabuleiro[LINHA][COLUNA];
+
+void jogar(char matriz[][COLUNA]);
 void tela(const char b[][COLUNA], int player);
 void iniciaTabuleiro(char tab[][COLUNA]);
+int coordenadaEhValida(int x, int y);
+int eValido(char letra);
+int jogador();
+
 
 int main() {
 
-	char tabuleiro[LINHA][COLUNA];
 	iniciaTabuleiro(tabuleiro);
-
-	tela(tabuleiro, 1);
+	jogar(tabuleiro);
 
 
 	return 0;
+}
+
+void jogar(char matriz[][COLUNA]) {
+	int play = 1, jogadas = 0, ganhou = 0;
+	int x, y, valida;
+	do {
+		do {
+
+			tela(matriz, play);
+			printf("Escolha uma posicao!\n");
+			printf("Linha 1,2,3:\n");
+			scanf("%d", &x);
+			printf("Coluna A,B,C:\n");
+			scanf("%d", &y);
+
+			valida = coordenadaEhValida(x, y);
+			if (valida == 1) {
+				valida += posicaoVazia(x, y);
+			}
+		} while (valida != 2);
+		if (play == 1) {
+			matriz[x][y] = 'X';
+		}
+		else {
+			matriz[x][y] = 'O';
+		}
+		jogadas++;
+		play++;
+		if (play == 3) {
+			play = 1;
+		}
+	} while (ganhou == 0 && jogadas < 9);
+}
+
+int posicaoVazia(int x, int y) {
+	if (tabuleiro[x][y] != 'X' && tabuleiro[x][y] != 'O') {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+
+}
+
+int coordenadaEhValida(int x, int y) {
+
+	if (x >= 0 && x < LINHA) {
+		if (y >= 0 && y < 3) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int jogador() {
+	
+}
+
+
+
+int eValido(char letra) {
+	if (letra == 'X' || letra == 'O')
+		return 1;
+	else
+		return 0;
 }
 
 void iniciaTabuleiro(char tab[][COLUNA]) {
@@ -52,5 +122,4 @@ void tela(const char b[][COLUNA], const int player) {
 		if (linha < LINHA - 1)
 			printf("           ---------\n");
 	}
-	printf("\n Escolha uma posicao: \n");
 }
